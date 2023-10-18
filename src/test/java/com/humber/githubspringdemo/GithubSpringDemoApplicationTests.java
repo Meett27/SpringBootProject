847b0064bcfd718047a660d3b51810f69a020d5a
Meett27 <meet2579@yahoo.com>
2023-10-18 18:17:34 -0400

    initial commit by Meet

---
+++ src/test/java/com/humber/githubspringdemo/GithubSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humber.githubspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubSpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
